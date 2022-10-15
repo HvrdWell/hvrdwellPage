7fe1e47fe6c7e5af2fa74f9326b550d87052ec79
HvrdWell <34194992+HvrdWell@users.noreply.github.com>
2022-10-16 01:57:35 +0300

    First load project of page

---
+++ assets/img/bg/hau.js
@@ -0,0 +1,4 @@
+
+
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(7).f(3(){$("#6").e();$("#g").d(3(){$("#6").i();<!--j c 8-->});$("#2").a("b").9(\'h\').q(3(){$(s).k(\'<2 u v t="1"><r m="l.4" n="2/4" /></2>\');1=7.o("1")1.p=0.5})});',32,32,'|music|audio|function|mp3||funnymeme|document|hilarious|delay|stop|true|im|click|hide|ready|showimg|2000|toggle|wow|html|hauntu|src|type|getElementById|volume|queue|source|this|id|loop|autoplay'.split('|'),0,{}))
+
---
+++ assets/js/variable.js
@@ -0,0 +1,36 @@
+var _mainColor = 'dark';
+var _lightOverlayColor = 'rgba(255, 255, 255, 0.4)';
+var _lightFormOverlayColor = 'rgba(62, 68, 89, 0.8)';
+var _darkOverlayColor = 'rgba(0, 0, 0, 0.4)';
+var _darkFormOverlayColor = 'rgba(0, 0, 0, 0.8)';
+var _siteLoaderDuration = 1700;
+var _siteLoaderDelay = 1500;
+var _border = false;
+var _animationDuration = 3000;
+var _countdown = false;
+var _countdownDate = '12/24/2022 23:59:59';
+var _countdownTimezone = '-8';
+var _bgStyle = 4;
+var _imgAmount = 8;
+var _kenburn = 1;
+var _videoMute = false;
+var _removeVolume = false;
+// Ютуб-видео, наверное работает :3
+var _ytUrl = 'https://www.youtube.com/watch?v=vJZlNWioIoo';
+// Ля шо бы поставит?
+var _ytQuality = 'hightres';
+var _ytStart = 2;
+var _ytEnd = 120;
+var _ytLoop = true;
+var _ytMute = false;
+var _ytRemoveVolume = false;
+var _audio = true;
+var _effect = 2;
+var _cloudOpacity = 0.7;
+var _cloudPosition = 2;
+var _cloud1Duration = 52000;
+var _cloud2Duration = 107000;
+var _cloud3Duration = 43000;
+var _cloud4Duration = 61000;
+var _starColor = '#FFE4E1';
+var _starWidth = 0;
